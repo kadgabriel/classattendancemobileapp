bedf4dacb2390683eb32a008f3897f26d4428506
Oliver Atienza <oliatienza@gmail.com>
2018-02-08 20:26:28 +0800

    Added the view (empty) class functionality

---
+++ app/src/main/java/com/example/classattendancemobileapp/ViewClassActivity.java
@@ -0,0 +1,13 @@
+package com.example.classattendancemobileapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewClassActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.view_class_activity);
+    }
+}
