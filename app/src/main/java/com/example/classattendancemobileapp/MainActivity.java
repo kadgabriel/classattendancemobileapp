f420db72ad5f72e2f4903ebc92ef0cd8bf519db2
Oliver Atienza <oliatienza@gmail.com>
2018-02-08 21:33:54 +0800

    Added the view (empty) class functionality

--- app/src/main/java/com/example/classattendancemobileapp/MainActivity.java
+++ app/src/main/java/com/example/classattendancemobileapp/MainActivity.java
@@ -2,12 +2,11 @@ package com.example.classattendancemobileapp;
 
 import android.arch.persistence.room.Room;
 import android.content.Intent;
-import android.os.Debug;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -27,9 +26,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         buildDB();
-        classListView = (ListView)findViewById(R.id.classListView);
-        classController = new ClassController(db, getApplicationContext());
 
+        classListView = (ListView)findViewById(R.id.classListView);
+        addClassFAB = (FloatingActionButton) findViewById(R.id.addClassFAB);
+
+        classController = new ClassController(db, getApplicationContext());
         classController.insertClass("", "computerscience192");
         classController.insertClass("CS193", "computerscience192");
         classController.insertClass("CS194", "computerscience192");
@@ -38,15 +39,26 @@ public class MainActivity extends AppCompatActivity {
         classController.insertClass("CS197", "computerscience192");
         classController.insertClass("CS198", "computerscience192");
 
+        //Get info of classes from the database
         List<Classes> classList = classController.getAllClasses();
-        String[] classNames = new String[classList.size()];
+        final String[] classNames = new String[classList.size()];
         for(int i = 0; i < classList.size(); i++){
             classNames[i] = classList.get(i).getClassName();
         }
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, classNames);
+
         classListView.setAdapter(adapter);
-        addClassFAB = (FloatingActionButton) findViewById(R.id.addClassFAB);
+        //Added listview item click listener
+        classListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(getApplicationContext(), ViewClassActivity.class);
+                intent.putExtra("CLASS_NAME", classNames[(int) l]);
+                startActivity(intent);
+            }
+        });
+
         addClassFAB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -60,7 +72,7 @@ public class MainActivity extends AppCompatActivity {
         db = Room.inMemoryDatabaseBuilder(
                 getApplicationContext(),
                 AppDatabase.class).allowMainThreadQueries().build();
-//                    db = Room.databaseBuilder(getApplicationContext(),
-//                              AppDatabase.class, "classattendance").allowMainThreadQueries().build();
+//      db = Room.databaseBuilder(getApplicationContext(),
+//              AppDatabase.class, "classattendance").allowMainThreadQueries().build();
     }
 }
