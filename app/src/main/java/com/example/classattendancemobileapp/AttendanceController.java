38828249dabedf89df4ccc526ad091c2b5f6d1fc
rbaustria <rbaustria@users.noreply.github.com>
2018-03-09 13:06:57 +0800

    Added code history and comments

--- app/src/main/java/com/example/classattendancemobileapp/AttendanceController.java
+++ app/src/main/java/com/example/classattendancemobileapp/AttendanceController.java
@@ -1,3 +1,38 @@
+/**
+ * Class Attendance Mobile App
+ *
+ * “This is a course requirement for CS 192 Software Engineering II
+ * under the supervision of Asst. Prof. Ma. Rowena C. Solamo
+ * of the Department of Computer Science, College of Engineering,
+ * University of the Philippines, Diliman for the AY 2017-2018”.
+ *
+ * @File Author(s): Ronnel Austria, Arielle Gabriel
+ *
+ * */
+
+/**
+ * Code History
+ *    Version x.x <DD/MM/YYYY> - Author
+ *         [description of changes]
+ * Version 1.0 <07/03/2018> - Ronnel Austria
+ *    - created initial file for attendance controller
+ * Version 1.01 <07/03/2018> - Arielle Gabriel
+ *    - minor changes in checking existing attendance records
+ * */
+
+/**
+ * Class Attendance Mobile App
+ *
+ * Class Attendance Mobile App is a mobile application that allows the teacher to record
+ * the attendance​ of the students​ digitally​ using a smart phone
+ *
+ * @Group members: Atienza, Austria, Gabriel
+ * @Client: Asst. Prof. Ma. Rowena C. Solamo
+ * @File:  AttendanceController.java
+ * @Creation Date: 07/03/18
+ * @Version: 1.01
+ *
+ * */
 package com.example.classattendancemobileapp;
 
 import android.content.Context;
@@ -16,6 +51,16 @@ public class AttendanceController {
      public AttendanceController(Context context){
         this.context = context;
     }
+
+     /**
+     * addAttendance() <07/03/2018>
+     * - add attendance controller
+     * @param: classID - classID of the class to check attendance, attendanceDate - date of the attendance record, attendanceList - list containing the studentnumber and
+      *                   attendance entry(Present, Late, Absent) of the student
+     * @requires: none
+     * @returns: Boolean value of true is operation is successful and false if otherwise
+     */
+
 
      public Boolean addAttendance(int classID, String attendanceDate, List<String[]> attendanceList){
           for(int i = 0; i<attendanceList.size(); i++){
